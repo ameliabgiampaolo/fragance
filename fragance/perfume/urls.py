5d46da99771a9f1b4ee023dc5c58f9e29da1b5ba
Amelia Brito <ameliagiampaolo@gmail.com>
2020-07-29 14:39:05 -0400

    pago listo

--- fragance/perfume/urls.py
+++ fragance/perfume/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('seleccion', views.seleccion, name='seleccion'),
     path('compra/<int:id_productor>/<int:id_proveedor>', views.compra, name='compra'),
-    path('pedido/<int:id_esencia>', views.pedido, name='pedido'),
+    path('pago', views.pago, name='pago'),
+    path('save/<int:id_pedido>', views.save, name='save'),
 ]
--- fragance/perfume/views.py
+++ fragance/perfume/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
 from .forms import ProductorForm, ProveedorForm
-from .models import vam_contrato, vam_elemento_contrato, vam_productor, vam_proveedor, vam_ingrediente_esencia, vam_ingrediente_otro
+from .models import vam_contrato, vam_elemento_contrato, vam_productor, vam_proveedor, vam_ingrediente_esencia, vam_ingrediente_otro, vam_pedido, vam_pago
+import datetime
 
 def index(request):
     context = {} 
     return render(request, 'perfume/index.html', context)
+
+def next_val(model):
+    tabla = model.objects.all()
+    return len(tabla) + 1
 
 def seleccion(request):
     if request.method == 'POST':
@@ -24,15 +29,42 @@ def seleccion(request):
 def compra(request, id_productor, id_proveedor):
     contrato = vam_contrato.objects.get(id_productor=id_productor, id_proveedor=id_proveedor)
 
-    elemento = vam_elemento_contrato.objects.filter(id_contrato=contrato).values('id_ingrediente_esencia_id')
+    elemento = vam_elemento_contrato.objects.filter(id_contrato=contrato).values('id_ingrediente_esencia_id', 'id_ingrediente_otro_id')
+    otro = None
 
     for e in elemento:
         esencia = vam_ingrediente_esencia.objects.get(id_ingrediente_esencia = e['id_ingrediente_esencia_id'])
-
-    
-
-    context = {'contrato': contrato, 'elemento': elemento, 'esencia': esencia}
+        if e['id_ingrediente_otro_id'] != None:
+            esencia_otro = vam_ingrediente_otro.objects.get(id_ingrediente_otro = e['id_ingrediente_otro_id'])
+            otro = True
+        
+    if otro:
+        context = {'contrato': contrato, 'elemento': elemento, 'esencia': esencia, 'esencia_otro': esencia_otro}
+    else:
+        context = {'contrato': contrato, 'elemento': elemento, 'esencia': esencia}
     return render(request, 'perfume/compra.html', context)
 
-def pedido(request, esencia_id):
-    pass
+def pago(request):
+    pedido = vam_pedido.objects.filter(estatus='pendiente')
+    now = datetime.date.today()
+    
+    context = {'pedido': pedido}
+    return render(request, 'perfume/pago.html', context)
+
+def save(request, id_pedido):
+    pedido = vam_pedido.objects.get(id_pedido=id_pedido)
+    now = datetime.date.today()
+
+    pago = vam_pago()
+    pago.id_pago = next_val(vam_pago)
+    pago.fecha_pago = now
+    pago.monto_pagado = pedido.precio_total
+    pago.id_pedido = pedido
+    pedido.estatus = 'procesado'
+    pago.save()
+    pedido.save()
+
+    pedido = vam_pedido.objects.filter(estatus='pendiente')
+    
+    context = {'pedido': pedido}
+    return render(request, 'perfume/pago.html', context)
