2ef6c4b715b229ab2f9c3dda36e346e3a1209a6d
Amelia Brito <ameliagiampaolo@gmail.com>
2020-07-29 00:02:56 -0400

    vista index

--- fragance/perfume/views.py
+++ fragance/perfume/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+def index(request):
+    context = {} 
+    return render(request, 'perfume/index.html', context)
+
+
